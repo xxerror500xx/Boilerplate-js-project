e17e68d5d95b9f842e1d6fb55c39ff2795c1e8dd
Cliff Minks <cliff.minks@gmail.com>
2018-04-06 08:08:01 -0500

    Add multiple bundles and create a dist clean up task.

--- Readme.md
+++ Readme.md
@@ -1,5 +1,14 @@
 A Starter Pack for a simple JS testing suite.
-  - Webpack
-  - Lodash
+  - Webpack (Build and task runner)
+  - Lodash (External js library Example)
+  - Asset build pipeline
+    - CSS
+    - Fonts
+    - Images
+  - HtmlWebpackPlugin
+  - clean-webpack-plugin
+  - Creates a Multiple Bundle builds
+
+[TODO'S]
   - Jasmine
   - WebdriverIO
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import _ from 'lodash';
+import printMe from './print.js';
 import './style.css';
 import Icon from './icon.png';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+  
   // Add the image to our existing div.
   var myIcon = new Image();
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
-  
+
   return element;
 }
 
